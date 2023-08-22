f658e766aa9d8fe7c8380d3a7c2a405c55b20f4a
Wout Schellaert <wout.schellaert@gmail.com>
2023-08-22 15:50:18 +0200

    Clean up readme and train script

--- README.md
+++ README.md
@@ -7,8 +7,10 @@
 3. Install dreamerv3 separately (installing together makes conda download jax without GPU support)
    `pip install "git+https://github.com/Kinds-of-Intelligence-CFI/dreamerv3-compat.git#egg=dreamerv3" numpy==1.21.2`
    Note: We explicitly ask for `numpy==1.21.1`, as pip overrides conda installed version (but others are not compatible with ml-agents).
-4. Run all the tests in the `tests/` dir to sanity check the installation. It is recommended starting with `python tests/jaxtest.py` and then moving on to `dreamertest.py`, followed by `aaitest.py` and lastly `integration.py`. If things don't crash, they work. Also watch for the errors. You can set `no_graphics=True` if working in a headless environment (but expect many warning messages from Unity).
-5. Adapt the `integration.py` script to your training needs.
+4. Run `python tests/jaxtest.py` and `CUDA_VISIBLE_DEVICES=0 python tests/dreamertest.py` to sanity check the installation. We set only the 0'th (i.e. the first) GPU visible because DreamerV3 does not explicitly support multi GPU and we don't want to hog more than necessary. Not needed if there is only 1 GPU available.
+5. Download the [AnimalAI environment](https://github.com/Kinds-of-Intelligence-CFI/animal-ai#quick-install-please-see-release-for-latest-version-of-aai-3). The current train script will look for the env at `./aai/env/AnimalAI.x86_64`, but there is an `--aai` flag available to look in different locations, or you can just modify the train script.
+6. Test installation of AnimalAI by running `python aaitest.py`, which should spawn a random AnimalAI arena playable by you through the keyboard, and run the integration test `CUDA_VISIBLE_DEVICES=0 python tests/integration.py integration.py`. If things don't crash, they likely work. Also watch for the errors.
+7. Adapt the `train.py` script to your training needs.
 
 ## References
 
--- train.py
+++ train.py
@@ -1,35 +1,67 @@
 import os
 # Make sure this is above the import of AnimalAIEnvironment
 os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"  # noqa
-os.environ['CUDA_VISIBLE_DEVICES'] = '0' # noqa
-# os.environ['CUDA_VISIBLE_DEVICES'] = '1' # noqa
 
+import random
+import argparse
 from pathlib import Path
-import logging
 from datetime import datetime
+from dataclasses import dataclass
+
+import logging
 logging.basicConfig(  # noqa
-    # format='[%(asctime)s] [%(levelname)-8s] [%(pathname)s] %(message)s',
     format='[%(asctime)s] [%(levelname)-8s] [%(module)s] %(message)s',
     level=logging.INFO
 )
-
-import random
 
 import dreamerv3
 from dreamerv3 import embodied
 from dreamerv3.embodied.envs import from_gym
 from gym.wrappers.compatibility import EnvCompatibility
-
 from mlagents_envs.envs.unity_gym_env import UnityToGymWrapper
 from animalai.envs.environment import AnimalAIEnvironment
 
+@dataclass
+class Args:
+    task: Path
+    aai: Path
+    # TODO: Checkpoint restart
 
-def get_dreamer_config(run_logdir):
+
+def run(args: Args):
+    task_path = args.task
+    task_name = Path(args.task).stem
+    date = datetime.now().strftime("%Y_%m_%d_%H_%M")
+    logdir = Path("./logdir/") / f'training-{date}-{task_name}'
+
+    logging.info("Creating DreamerV3 config")
+    dreamer_config, step, logger = get_dreamer_config(logdir)
+
+    logging.info(f"Creating AAI Dreamer Environment")
+    env = get_aai_env(task_path, args.aai, dreamer_config)
+
+    logging.info("Creating DreamerV3 Agent")
+    agent = dreamerv3.Agent(env.obs_space, env.act_space, step, dreamer_config)
+    replay = embodied.replay.Uniform(
+        dreamer_config.batch_length, 
+        dreamer_config.replay_size,
+        logdir / 'replay')
+    emb_config = embodied.Config(
+        **dreamer_config.run, 
+        logdir=dreamer_config.logdir,
+        batch_steps=dreamer_config.batch_size * dreamer_config.batch_length)  # type: ignore
+
+    logging.info("Starting training")
+    embodied.run.train(agent, env, replay, logger, emb_config)
+    # embodied.run.eval_only(agent, env, logger, args)
+
+
+def get_dreamer_config(logdir: Path):
     # See configs.yaml for all options.
     config = embodied.Config(dreamerv3.configs['defaults'])
     config = config.update(dreamerv3.configs['xlarge'])
     config = config.update({
-        'logdir': run_logdir,
+        'logdir': logdir,
         'run.train_ratio': 64,
         'run.log_every': 60,  # Seconds
         'batch_size': 16,
@@ -44,7 +76,6 @@ def get_dreamer_config(run_logdir):
 
     step = embodied.Counter()
 
-    logdir = embodied.Path(config.logdir)
     logger = embodied.Logger(step, [
         embodied.logger.TerminalOutput(),
         embodied.logger.JSONLOutput(logdir, 'metrics.jsonl'),
@@ -53,15 +84,12 @@ def get_dreamer_config(run_logdir):
         # embodied.logger.MLFlowOutput(logdir.name),
     ])
 
-    return config, step, logger, logdir
+    return config, step, logger
 
 
-def aai_env(task_path, dreamer_config, logdir):
-    # use a random port to avoid problems if a previous version exits slowly
+def get_aai_env(task_path, env_path, dreamer_config):
+    # Use a random port to avoid problems if a previous version exits slowly
     port = 5005 + random.randint(0, 1000)
-
-    # env_path = "./aai/env3.0.1/AAI_v3.0.1_build_linux_090422.x86_64"
-    env_path = "./aai/env3.0.2/AAI3Linux.x86_64"
 
     logging.info("Initializing AAI environment")
     aai_env = AnimalAIEnvironment(
@@ -71,55 +99,26 @@ def aai_env(task_path, dreamer_config, logdir):
         # Set pixels to 64x64 cause it has to be power of 2 for dreamerv3
         resolution=64, # same size as Minecraft in DreamerV3
     )
-    logging.info("Applying UnityToGymWrapper")
     env = UnityToGymWrapper(
         aai_env, uint8_visual=True,
         # allow_multiple_obs=True, # This crashes somewhere in one of the wrappers.
         flatten_branched=True)  # Necessary. Dreamerv3 doesn't support MultiDiscrete action space.
-
-    logging.info("Applying EnvCompatibility")
     env = EnvCompatibility(env, render_mode='rgb_array')  # type: ignore
-
-    logging.info("Applying DreamerV3 FromGym")
     env = from_gym.FromGym(env, obs_key='image')
     logging.info(f"Using observation space {env.obs_space}")
     logging.info(f"Using action space {env.act_space}")
-
-    logging.info("Wrapping DreamerV3 environment")
     env = dreamerv3.wrap_env(env, dreamer_config)
-    logging.info("Creating BatchEnv")
     env = embodied.BatchEnv([env], parallel=False)
 
     return env
 
 
-def main():
-    # task_config = "./aai/configs/OP-Controls-RandBasic-SanityGreen-RND-RND-NA-RND-NA.yml"
-    # task_config = "./aai/configs/paper/SanityMovingYellowMulti10.yml"
-    # task_config = "./aai/configs/paper/SanityMovingYellow.yml"
-    task_config = "./aai/configs/paper/SanityYellowMulti10.yml"
-    task_name = Path(task_config).stem
-
-    date = datetime.now().strftime("%Y_%m_%d_%H_%M")
-    logdir = Path("./logdir/") / f'training-{date}-{task_name}'
-
-    logging.info("Creating DreamerV3 config")
-    dreamer_config, step, logger, logdir = get_dreamer_config(logdir)
-    logging.info(f"Creating AAI Dreamer Environment")
-    env = aai_env(task_config, dreamer_config, logdir)
-
-    logging.info("Creating DreamerV3 Agent")
-    agent = dreamerv3.Agent(env.obs_space, env.act_space, step, dreamer_config)
-    replay = embodied.replay.Uniform(
-        dreamer_config.batch_length, dreamer_config.replay_size, logdir / 'replay')
-    args = embodied.Config(
-        **dreamer_config.run, logdir=dreamer_config.logdir,
-        batch_steps=dreamer_config.batch_size * dreamer_config.batch_length)  # type: ignore
-
-    logging.info("Starting training")
-    embodied.run.train(agent, env, replay, logger, args)
-    # embodied.run.eval_only(agent, env, logger, args)
-
-
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--task', type=Path, required=True)
+    parser.add_argument('--aai', type=Path, default=Path('./aai/env/AnimalAI.x86_64'))
+    args_raw = parser.parse_args()
+
+    args = Args(**vars(args_raw))
+
+    run(args)
