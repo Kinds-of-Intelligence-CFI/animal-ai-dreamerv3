2d119c57fb4b2200d06a00c9da6807e5a857ee39
Wout Schellaert <wout.schellaert@gmail.com>
2023-11-15 15:57:53 +0100

    Remove stepbystep logger

--- train.py
+++ train.py
@@ -117,9 +117,6 @@ def run(args: Args):
     logging.info("Creating AAI Dreamer Environment")
     env = get_aai_env(task_path, args.env, dreamer_config)
 
-    # Add stepwise CSV logger
-    # env = StepwiseCSVLogger(env, Path(str(logdir)))
-
     logging.info("Creating DreamerV3 Agent")
     agent = dreamerv3.Agent(env.obs_space, env.act_space, step, dreamer_config)
 
@@ -219,9 +216,6 @@ def get_aai_env(
     return env
 
 
-# ----------- Utils
-
-
 class MultiObsWrapper(gym.ObservationWrapper):  # type: ignore
     """
     Go from tuple to dict observation space.
@@ -234,71 +228,17 @@ class MultiObsWrapper(gym.ObservationWrapper):  # type: ignore
         tuple_obs_space: gym.spaces.Tuple = self.observation_space  # type: ignore
         self.observation_space = gym.spaces.Dict(
             {
+                # RGB image
                 "image": tuple_obs_space[0],
-                "extra": tuple_obs_space[
-                    1
-                ],  # Health, velocity (x, y, z), and global position (x, y, z)
+                # Health, velocity (x, y, z), and global position (x, y, z)
+                # in a 1D array of 7 entries.
+                "extra": tuple_obs_space[1],
             }
         )
 
     def observation(self, observation):
         image, extra = observation
         return {"image": image, "extra": extra}
-
-
-class StepwiseCSVLogger(embodied.BatchEnv):
-    """
-    Logs the environment after every step to a CSV file.
-
-    <https://stackoverflow.com/questions/1443129/completely-wrap-an-object-in-python>
-    """
-
-    def __init__(
-        self, env: embodied.BatchEnv, logdir: Path, episode: int = 1, step: int = 1
-    ):
-        self.__env = env
-        self.__logdir = logdir / "episodes"
-        self.__logdir.mkdir(parents=True, exist_ok=False)
-
-        self.__episode = episode
-        self.__stepnum = step
-
-        self.__open_episode_log()
-
-    def __getattr__(self, __name) -> Any:
-        return getattr(self.__env, __name)
-
-    def __open_episode_log(self):
-        path = self.__logdir / f"episode_{self.__episode}.csv"
-        path.touch()
-        self.__csv_file = path.open("w")
-        self.__csv_file.write(
-            "episode, step, reward, done, health, vx, vy, vz, px, py, pz\n"
-        )
-
-    def step(self, action):
-        step_result = self.__env.step(
-            action
-        )  # We still need to unwrap the BatchEnv output
-        extra = step_result["extra"][0]  # This is the info we care about
-        reward = step_result["reward"][0]
-        done = step_result["is_last"][0]
-
-        log_extra = ", ".join([str(x) for x in extra])
-        self.__csv_file.write(
-            f"{self.__episode}, {self.__stepnum}, {reward}, {done}, {log_extra}\n"
-        )
-
-        self.__stepnum += 1
-        if done:
-            self.__episode += 1
-            self.__open_episode_log()
-
-        return step_result
-
-    def close(self):
-        self.__csv_file.close()
-        return self.__env.close()
 
 
 if __name__ == "__main__":
